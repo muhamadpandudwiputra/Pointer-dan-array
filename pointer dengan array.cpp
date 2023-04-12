daabff540f6346e8bd49907c8a63397135a9868a
muhamadpandudwiputra <112594630+muhamadpandudwiputra@users.noreply.github.com>
2023-04-12 21:54:32 +0700

    Add files via upload

---
+++ pointer dengan array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int arr[5] = {10, 20, 30,  40, 50};
+	int *ptr = arr; //ptr menunjuk ke alamat memori awal dari array
+	
+	for(int i = 0; i < 5; i++){
+		cout << "Value of arr[" << i << "] = " << *ptr << endl;
+		ptr++; // pindah ke alamat memori berikutnya
+	}
+	
+	return 0;
+}
